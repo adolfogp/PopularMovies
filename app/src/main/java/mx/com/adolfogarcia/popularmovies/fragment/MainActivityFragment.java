f7008368ebb31b1138a9886188dc235e24fda2c8
Jesús Adolfo García Pasquel <adolfogp@gmail.com>
2015-08-31 21:29:29 -0500

    * First commit.

---
+++ app/src/main/java/mx/com/adolfogarcia/popularmovies/fragment/MainActivityFragment.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Jesús Adolfo García Pasquel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mx.com.adolfogarcia.popularmovies.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mx.com.adolfogarcia.popularmovies.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ *
+ * @author Jesús Adolfo García Pasquel
+ */
+public class MainActivityFragment extends Fragment {
+
+    /**
+     * Creates a new instance of {@link MainActivityFragment}.
+     */
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
